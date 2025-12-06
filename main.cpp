290f100522e4a3a52b4bbad6e567e90da39fa03a
Leo <znone666@gmail.com>
2025-12-06 10:52:04 +0700

    Add input output loop

--- main.cpp
+++ main.cpp
@@ -1,41 +1,81 @@
 #include<iostream>
 #include<cstdlib>
 #include<fstream>
-using std::cout;
-using std::endl;
+#include<string>
+#include<vector>
+using namespace std;
 
-void usage(){
+void printUsage(){
 		cout << "usage" << endl;
 }
 
-void executeAbbrCommands(std::string commands){
+void stringifyArgs(char** argv, int argc, vector<string>& args){
+		for (int i = 0; i < argc; i++){
+				args.push_back(string(argv[i]));
+		}
+}
+
+// multiple shell environments not implemented
+bool executeCommand(char c){
+		switch(c){
+				case 'l':
+						system("ls");
+						return true;
+				case 'g':
+						system("git log");
+						return true;
+				case 'p':
+						system("pwd");
+						return true;
+				case 'h':
+						printUsage();
+						return true;
+				default:
+						return false;
+		}
+}
+
+void executeAbbrCommands(string commands){
 		for (char command : commands) {
-				switch (command){
-						case 'l':
-								system("ls");
-								break;
-						case 'g':
-								system("git log");
-								break;
-						case 'p':
-								system("pwd");
-								break;
-						default:
-								cout << "unimplemented commnand `" << command << "`" << endl;
-								break;
+				if (!executeCommand(command)) {
+						cout << "error: invalid command `" << command << "`" << endl;
+				}
+		}
+}
+
+void loopMode(){
+		string command;
+		while (true){
+				cout << "> ";
+				getline(cin >> ws, command);
+				if (command == "help" || command == "h"){
+						printUsage();
+				} else if (command == "exit" || command == "ex"){
+						break;
+				}
+				else {
+						cout << command << endl;
 				}
 		}
 }
 
 int main(int argc, char** argv){
+		vector<string> args;
+		stringifyArgs(argv, argc, args);
+
 		if (argc == 1) {
-				usage(); return 0;
+				printUsage(); return 0;
 		} else if (argc == 2){
-				std::string abbrCommands(argv[1]);
-				executeAbbrCommands(abbrCommands);
+				if (args[1] == "-m"){
+						loopMode();
+				} else {
+						string abbrCommands(argv[1]);
+						executeAbbrCommands(abbrCommands);
+				}
 		} else {
 				cout << "ERROR: Invalid command" << endl;
 				return 1;
 		}
+
 		return 0;
 }
