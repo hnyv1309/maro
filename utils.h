fb7728ab6b1dd44a712fab1bb0256f14ad0dfc1f
Leo <znone666@gmail.com>
2025-12-08 19:09:34 +0700

    Modulization

---
+++ utils.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<vector>
+#include<string>
+
+using namespace std;
+
+enum LoopFlag {
+		LF_UNDEFINED,
+		LF_EXIT,
+		LF_SUCCESS
+};
+
+
+void printUsage();
+void printTmuxWorkspaces();
+void stringifyArgs(char** argv, int argc, vector<string>& args);
+bool isWhiteSpace(char a);
+void stringSplit(string target, vector<string>& output);
+bool executeAbbrCommand(char c);
+LoopFlag excuteLoop(vector<string> tokens);
+void executeAbbrCommands(string commands);
+void loopMode();
